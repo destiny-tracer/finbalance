62b3165fb3a4a01a5a82154fe4634321d39afb5d
destiny-tracer <spiritrident@gmail.com>
2025-11-14 22:50:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/finbalance/', // if deploying to GitHub Pages
   plugins: [react()],
-  base: '/finbalance/' // if deploying to GitHub Pages
-});
+  });
